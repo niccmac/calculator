60b29975bb2bae272332d7933370b52bf612bcd6
Nicole MacLean <nicole.mac0404@gmail.com>
2022-06-01 11:48:27 -0700

    Add fubnctionality

--- src/App.js
+++ src/App.js
@@ -9,39 +9,159 @@ function App() {
   // Operators
   const ops = ["/", "*", "+", "-", "."];
 
-  // Calculator function
-
+  // Add numbers and operators
   const updateCalc = (value) => {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
     setCalc(calc + value);
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString());
+    }
+  };
+
+  // Calculate
+
+  const calaculator = () => {
+    setCalc(eval(calc).toString());
+  };
+
+  // Delete last
+
+  const deleteLast = () => {
+    if (calc === "") {
+      return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
   };
 
   return (
     <div className="App">
       <div className="calaculator">
         <div className="calc-display">
-          <span>(0) </span>
-          {calc}
+          {result ? <span>({result})</span> : ""}&nbsp;
+          {calc || "0"}
         </div>
         <div className="calc-operators">
-          <button>/</button>
-          <button>*</button>
-          <button>+</button>
-          <button>-</button>
-          <button>DELETE</button>
+          <button
+            onClick={() => {
+              updateCalc("/");
+            }}
+          >
+            /
+          </button>
+          <button
+            onClick={() => {
+              updateCalc("*");
+            }}
+          >
+            *
+          </button>
+          <button
+            onClick={() => {
+              updateCalc("+");
+            }}
+          >
+            +
+          </button>
+          <button
+            onClick={() => {
+              updateCalc("-");
+            }}
+          >
+            -
+          </button>
+
+          <button onClick={deleteLast}>DELETE</button>
         </div>
         <div className="calc-numbers">
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
+          <button
+            onClick={() => {
+              updateCalc("0");
+            }}
+          >
+            0
+          </button>
+          <button
+            onClick={() => {
+              updateCalc(".");
+            }}
+          >
+            .
+          </button>
+
+          <button onClick={calaculator}>=</button>
+
+          <button
+            onClick={() => {
+              updateCalc("1");
+            }}
+          >
+            1
+          </button>
+          <button
+            onClick={() => {
+              updateCalc("2");
+            }}
+          >
+            2
+          </button>
+          <button
+            onClick={() => {
+              updateCalc("3");
+            }}
+          >
+            3
+          </button>
+          <button
+            onClick={() => {
+              updateCalc("4");
+            }}
+          >
+            4
+          </button>
+          <button
+            onClick={() => {
+              updateCalc("5");
+            }}
+          >
+            5
+          </button>
+          <button
+            onClick={() => {
+              updateCalc("6");
+            }}
+          >
+            6
+          </button>
+          <button
+            onClick={() => {
+              updateCalc("7");
+            }}
+          >
+            7
+          </button>
+          <button
+            onClick={() => {
+              updateCalc("8");
+            }}
+          >
+            8
+          </button>
+          <button
+            onClick={() => {
+              updateCalc("9");
+            }}
+          >
+            9
+          </button>
+        </div>
+        <div className="clear-calc">
+          <button onClick={() => setCalc("")}>CLEAR</button>
         </div>
       </div>
     </div>
