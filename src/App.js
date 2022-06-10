27cae7f80caf6c2a700afab19c3afd7bdfbb8213
Nicole MacLean <nicole.mac0404@gmail.com>
2022-06-10 11:55:00 -0600

    Creating todo form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Calculator from "./components/calculator";
 import CardGame from "./components/cardGame";
+import ToDoForm from "./components/toDoForm";
 const App = () => {
   return (
     <div className="App">
       {/* <Calculator /> */}
       {/* <p>Duck</p> */}
-      <CardGame />
+      {/* <CardGame /> */}
+      <ToDoForm />
     </div>
   );
 };
--- src/components/toDoForm.jsx
+++ src/components/toDoForm.jsx
@@ -1,7 +1,27 @@
 import React, { useState } from "react";
+import { Button, TextField } from "@mui/material";
 
 const ToDoForm = () => {
-  const [input, setInput] = useState();
-  return <div></div>;
+  const [input, setInput] = useState("");
+  const inputHandler = (e) => {
+    e.preventDefault();
+    setInput(e.target.input);
+  };
+  return (
+    <>
+      <TextField className="todo-form" onSubmit={inputHandler}>
+        <input
+          type="text"
+          placeholder="Add a task..."
+          value={input}
+          name="text"
+          className="todo-input"
+        />
+      </TextField>
+      <Button className="todo-button" variant="outlined" type="submit">
+        Add Task
+      </Button>
+    </>
+  );
 };
 export default ToDoForm;
