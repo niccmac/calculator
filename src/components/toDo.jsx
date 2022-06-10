917bdd647d05f08fc376f854b54dffe673db6f73
Nicole MacLean <nicole.mac0404@gmail.com>
2022-06-10 13:35:48 -0600

    All task show with edit and delete buttton, need edit delete functionality

--- src/components/toDo.jsx
+++ src/components/toDo.jsx
@@ -1,1 +1,42 @@
+import React, { useState } from "react";
+import ToDoForm from "./toDoForm";
+import { AiTwotoneEdit, AiTwotoneDelete } from "react-icons/ai";
 
+const ToDo = (allTasks, completeToDo) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
+  let taskList;
+  const mapMe = allTasks.allTasks;
+  console.log(mapMe[0]);
+  if (Array.isArray(mapMe)) {
+    taskList = mapMe[0].map((task, index) => {
+      console.log(task);
+      return (
+        <div
+          // className={task[0].isComplete ? "todo-row complete" : "todo-row "}
+          key={index}
+        >
+          <div key={task.id} onClick={() => completeToDo(task.id)}>
+            {task.text}
+          </div>
+          <AiTwotoneEdit
+          // className="edit-icon"
+          // onClick={() => {
+          //   editToDo(todo.id);
+          // }}
+          />
+          <AiTwotoneDelete
+          // className="delete-icon"
+          // onClick={() => {
+          //   setEdit({ id: task.id, value: task.value });
+          // }}
+          />
+        </div>
+      );
+    });
+  }
+  return <div>{taskList}</div>;
+};
+export default ToDo;
--- src/components/toDoList.jsx
+++ src/components/toDoList.jsx
@@ -1,4 +1,6 @@
+import { ClassNames } from "@emotion/react";
 import React, { useState } from "react";
+import ToDo from "./toDo";
 import ToDoForm from "./toDoForm";
 const ToDoList = (props) => {
   const [allToDo, setAllToDo] = useState([]);
@@ -9,11 +11,21 @@ const ToDoList = (props) => {
     const newToDos = [...allToDo, todo];
     setAllToDo(newToDos);
   };
+  const completeToDo = (id) => {
+    let updatedToDos = allToDo.map((task) => {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+      return task;
+    });
+    setAllToDo([...updatedToDos]);
+  };
   return (
-    <>
+    <div className="todo">
       <h1>To Do</h1>
       <ToDoForm onSubmit={addToDo} />
-    </>
+      <ToDo allTasks={[allToDo]} completeToDo={completeToDo} />
+    </div>
   );
 };
 export default ToDoList;
