15afedc74ffdecb19d1ad68976fc35fcf1aaa383
Nicole MacLean <nicole.mac0404@gmail.com>
2022-06-10 12:10:42 -0600

    Add uuid, basic form complete

--- src/components/toDoForm.jsx
+++ src/components/toDoForm.jsx
@@ -1,26 +1,41 @@
 import React, { useState } from "react";
 import { Button, TextField } from "@mui/material";
+import { v4 as uuidv4 } from "uuid";
 
 const ToDoForm = () => {
   const [input, setInput] = useState("");
   const inputHandler = (e) => {
+    setInput(e.target.value);
+  };
+  const submitHandler = (e) => {
     e.preventDefault();
-    setInput(e.target.input);
+
+    // props.onSubmit({
+    //   id: uuidv4(),
+    //   text: input,
+    // });
+    setInput("");
   };
   return (
     <>
-      <TextField className="todo-form" onSubmit={inputHandler}>
-        <input
+      <form className="todo-form">
+        <TextField
           type="text"
           placeholder="Add a task..."
           value={input}
           name="text"
           className="todo-input"
+          onChange={inputHandler}
         />
-      </TextField>
-      <Button className="todo-button" variant="outlined" type="submit">
-        Add Task
-      </Button>
+        <Button
+          className="todo-button"
+          variant="outlined"
+          type="submit"
+          onClick={submitHandler}
+        >
+          Add Task
+        </Button>
+      </form>
     </>
   );
 };
